4b74bf1952debabd58a8c35a379b22483cacacdd
JuanCardona16 <jcardonabageth@gmail.com>
2023-01-11 22:19:47 -0500

    config tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,6 +10,9 @@ module.exports = {
 				"Almost-White": "hsl(0, 0%, 98%)",
 				"Medium-Gray": "hsl(0, 0%, 41%)",
 				"Almost-Black": "hsl(0, 0%, 8%)",
+			},
+			fontFamily: {
+				'Epilogue': ['Epilogue', 'sans-serif']
 			}
 		},
 	},
